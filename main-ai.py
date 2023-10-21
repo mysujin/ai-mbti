f66e229d570f46831fd4ca962771078675a5ef5f
mysujin <144642120+mysujin@users.noreply.github.com>
2023-10-21 15:57:39 +0900

    Add files via upload

---
+++ main-ai.py
@@ -0,0 +1,23 @@
+import streamlit as st
+
+from dotenv import load_dotenv
+load_dotenv()
+
+from langchain.chat_models import ChatOpenAI
+
+
+chat_model = ChatOpenAI()
+
+st.title('MBTI 박사')
+
+content = st.text_input('알고 싶은 MBTI 유형을 알려 주세요')
+
+if st.button('MBTI 유형'):
+    with st.spinner('처리 중  ...'):
+        result = chat_model.predict(content + "의 MBTI 유형을 설명해줘")
+        st.write(result)
+
+
+
+
+
